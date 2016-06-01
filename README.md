f0397e976b858700c3d18d55d6920fa691581e1b
NicoVillanueva <villanueva.arg@gmail.com>
2016-06-01 02:02:05 -0300

    better stats, config file support, readme rewrite, safety

--- README.md
+++ README.md
@@ -1,6 +1,26 @@
-# bombardier
-Script for spamming GET requests to an URL in parallel
+# Bombardier
+Script for spamming requests to an URL in parallel
 
-Usage:  
+## Config
+Bombardier supports providing it with a configuration file. Check out the example one to see how to build one. When adding blocks of text (`payload`, `headers` and `cookies` sections), REMEMBER to indent it.
+
+`headers` and `cookies` need to be JSON-valid. `payload` doesn't.
+
+You can provide it with a configuration file (`-c`/`--config` parameter) or directly with the URL to bombard (`-u`/`--url` parameter). In the latter case, GET requests will be performed.
+
+If both `--config` and `--url` parameters are provided, `--config` takes precedence.
+
+## Dependencies
+Normal dependencies are listed in `requirements.txt`. Install them using `pip install -r requirements.txt`
+
+If you also want nifty statistics (max, min, percentile, etc...) you also need `NumPy`: `pip install numpy`
+
+As always, it's recommended to use virtualenvs.
+
+## Usage
 `bombardier.py --url $url-to-hit --threads $concurrent-threads --requests $requests-each-thread-will-launch`
 
+or
+
+
+`bombardier.py --config $config-file --threads $concurrent-threads --requests $requests-each-thread-will-launch`
--- bombardier.py
+++ bombardier.py
@@ -1,5 +1,26 @@
 #!/usr/bin/python3
-import requests, requests.exceptions, threading, logging, argparse, time, sys, json
+import requests, threading, logging, argparse, time, sys, json, yaml, urllib, os, ast
+
+
+class TargetDefinition(object):
+    def __init__(self, target):
+        if os.path.isfile(target):
+            with open(target) as f:
+                c = yaml.load(f)
+                self.target = c.get("target")
+                self.method = c.get("method")
+                self.payload = c.get("payload") or None
+                self.cookies = ast.literal_eval(c.get("cookies")) if c.get("cookies") else None
+                self.headers = ast.literal_eval(c.get("headers")) if c.get("headers") else None
+        else:
+            self.target = target
+            self.method = "GET"
+            self.payload = None
+            self.headers = None
+            self.cookies = None
+    def __str__(self):
+        return "target: {}, method: {}, payload: {}, headers: {}, cookies: {}"\
+                .format(self.target, self.method, self.payload, self.headers, self.cookies)
 
 
 def parse_arguments():
@@ -9,6 +30,7 @@ def parse_arguments():
     options.add_argument('-u', '--url', type=str, required=True, help='URL to bombard')
     options.add_argument('-t', '--threads', type=int, required=True, help='Parallel threads to hit with')
     options.add_argument('-r', '--requests', type=int, required=True, help='Requests to do with each thread')
+    options.add_argument('-c', '--config', type=str, required=False, help='Request configuration file to use')
     options.add_argument('--timeout', type=int, required=False, default=5, help='Seconds after the requests timeout (default: 5)')
     options.add_argument('-l', '--logfile', type=str, required=False, default='bombardier.log', help='Where to write the log')
     options.add_argument('-d', '--dump', type=str, required=False, default=None, help='Dump workers\' results in a file')
@@ -37,21 +59,25 @@ def setup_logging(logfile):
     logging.getLogger('').addHandler(console)
 
 
-def do_requests(target, amount, ret_values, index, workers_log=None):
+def do_requests(config, amount, ret_values, workers_log=None):
     global TIMEOUT
     responses = []
     timings = []
     for i in range(amount):
         try:
-            resp = requests.get(target, timeout=TIMEOUT)
+            resp = requests.request(method=config.method,
+                                    url=config.target,
+                                    data=config.payload,
+                                    headers=config.headers,
+                                    cookies=config.cookies,
+                                    timeout=TIMEOUT)
         except requests.exceptions.Timeout:
             responses.append(requests.codes.TIMEOUT)
-            timings.append(0)
+            timings.append(TIMEOUT)
             logging.error("Request #%i in %s timed out" % (i, threading.currentThread().getName()))
             continue
         except requests.exceptions.ConnectionError:
-            responses.append(requests.codes.IM_A_TEAPOT)
-            timings.append(0)
+            responses.append("CONNECTION_ERROR")
             logging.error("Request #%i in %s had a connection error" % (i, threading.currentThread().getName()))
             continue
 
@@ -65,16 +91,17 @@ def do_requests(target, amount, ret_values, index, workers_log=None):
         timings.append(resp.elapsed.total_seconds())
         responses.append(resp.status_code)
     logging.info("Completed all requests for %s" % threading.currentThread().getName())
-    # Stupid way to return values from a threaded method, as Py can't implement a 'return'
-    ret_values.append({"responses": responses, "average": (sum(timings) / len(timings))})
+    # Workaround way to return values from a threaded method:
+    if len(timings) is not 0:
+        ret_values.append({"responses": responses, "average": (sum(timings) / len(timings))})
 
 
-def perform_attack(target_url, attack_method, worker_amount, worker_efforts, workers_log=None):
+def perform(config, threaded_method, worker_amount, worker_efforts, workers_log=None):
     start_time = time.time()
     threads = []
     worker_results = []
     for i in range(worker_amount):
-        t = threading.Thread(target=attack_method, args=(target_url, worker_efforts, worker_results, i))
+        t = threading.Thread(target=threaded_method, args=(config, worker_efforts, worker_results))
         t.setName("worker-%i" % i)
         t.setDaemon(True)
         threads.append(t)
@@ -89,6 +116,7 @@ def perform_attack(target_url, attack_method, worker_amount, worker_efforts, wor
 
     end_time = time.time()
 
+    logging.info("----- All done! -----")
     logging.info("Completed %i requests in %.2f seconds" % (args.requests * args.threads, end_time - start_time))
     if workers_log is not None:
         with open(workers_log) as f:
@@ -103,19 +131,33 @@ def print_statistics(worker_results):
             f.write(json.dumps(worker_results, sort_keys=True, indent=2, separators=(',', ': ')))
 
     def no_numpy_stats(results):
-        logging.info("Average response time: %f" % (sum(results) / len(results)))
+        logging.info("Average time: %f" % (sum(results) / len(results)))
+        wrs = []
+        [wrs.extend(wr.get("responses")) for wr in worker_results]
+        resps = {}
+        for r in wrs:
+            if r not in resps.keys():
+                resps[r] = 1
+            else:
+                resps[r] += 1
+        for r in resps.keys():
+            logging.info("- {amt} responses with code {code}".format(amt=resps.get(r), code=r))
+
 
     def numpy_stats(results):
-        logging.warn('Stats time:')
+        logging.info('Stats time!')
+        no_numpy_stats(results)
         a = np.array(results)
         logging.info("80th percentile: {}".format(np.percentile(results, 80)))
         logging.info("99th percentile: {}".format(np.percentile(results, 99)))
-        logging.info("Mean: {}".format(a.mean()))
         logging.info("Minimum: {}".format(a.min()))
         logging.info("Maximum: {}".format(a.max()))
         logging.info("Variance: {}".format(a.var()))
         logging.info("Standard Deviation: {}".format(a.std()))
 
+    if len(averages) is 0:
+        logging.warn("No requests really made. Nothing to do.")
+        sys.exit(0)
     try:
         import numpy as np
         numpy_stats(averages)
@@ -124,10 +166,22 @@ def print_statistics(worker_results):
         logging.warn("No NumPy module found, so no more statistics for you.")
 
 
+def is_valid_url(url, qualifying=None):
+    min_attributes = ('scheme', 'netloc')
+    qualifying = min_attributes if qualifying is None else qualifying
+    token = urllib.parse.urlparse(url)
+    return all([getattr(token, qualifying_attr)
+                for qualifying_attr in qualifying])
+
+
 if __name__ == '__main__':
     global TIMEOUT
     args = parse_arguments()
     TIMEOUT = args.timeout
     setup_logging(args.logfile)
-    results = perform_attack(args.url, do_requests, args.threads, args.requests)
+    sc = TargetDefinition(args.config or args.url)
+    if not is_valid_url(args.url):
+        logging.error("Malformed URL: {}".format(args.url))
+        sys.exit(1)
+    results = perform(sc, do_requests, args.threads, args.requests)
     print_statistics(results)
